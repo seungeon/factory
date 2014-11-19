00c585742b8e9f47d2adc7d5952271743375eae8
seungeon <seungeon.k@gmail.com>
2014-11-19 18:29:09 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var img3;
 var img4;
 var img5;
 var img6;
+var img7;
+var img8;
 
 
 function setup()
@@ -18,6 +20,9 @@ function setup()
     img4 = loadImage("bottomR.png");
     img5 = loadImage("bottomL.png");
     img6 = loadImage("bottomMid.png");
+    img7 = loadImage("midBoxCopy.png");
+    img8 = loadImage("cookie.png");
+   
 
 
 }
@@ -29,12 +34,22 @@ function draw()
 
     fill(237,112,50);
     noStroke();
+
+    var duration = 5000;
+    var timing = (new Date()%duration)/duration;
+
+
  
     image(img,254,0);
+    image(img7,204,499);
     image(img2,204,499);
     image(img3,495,451);
     image(img4,556,714);
     image(img5,0,700);
     image(img6,212,724);
- 
+
+    fill(0);
+    for(var i = -1 ; i <6 ; i++){
+    	image(img8, i*(100+img.width)+timing*(100+img.width),603);
+
     } 
