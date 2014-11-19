2a49c70c3be4d4f68464fdd8c3f96ebf5c7c8b63
seungeon <seungeon.k@gmail.com>
2014-11-19 18:18:25 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ var bg;
 var img;  
 var img2;
 var img3;
+var img4;
+var img5;
+var img6;
 
 
 function setup()
@@ -12,6 +15,10 @@ function setup()
     img = loadImage("sugarBox.png");
     img2 = loadImage("midBox.png");
     img3 = loadImage("midBox2.png");
+    img4 = loadImage("bottomR.png");
+    img5 = loadImage("bottomL.png");
+    img6 = loadImage("bottomMid.png");
+
 
 }
 
@@ -26,5 +33,8 @@ function draw()
     image(img,254,0);
     image(img2,204,499);
     image(img3,495,451);
+    image(img4,556,714);
+    image(img5,0,700);
+    image(img6,212,724);
  
     } 
