06d2826219c54b1d24227e8045d93398be69456b
seungeon <seungeon.k@gmail.com>
2014-11-19 17:19:41 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,6 +1,20 @@
+var bg;
+
+
+
 function setup()
 {
     createCanvas(641,901);
     bg = loadImage("bg.png");
 
+}
+
+
+function draw()
+{
+    background(bg);
+
+    fill(237,112,50);
+    noStroke();
+ 
 }
